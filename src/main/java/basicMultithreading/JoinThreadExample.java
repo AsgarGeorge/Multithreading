cf8802b5d93fec444d5059bb2b50d27201fc9cbf
Asgar_George <asgarantony@gmail.com>
2025-05-15 20:25:52 +0530

    comments

--- src/main/java/basicMultithreading/JoinThreadExample.java
+++ src/main/java/basicMultithreading/JoinThreadExample.java
@@ -1,5 +1,11 @@
 package basicMultithreading;
 
+/* What is join() ?
+ Imagine threads to be lines of execution. So, when we call •join() on a certain thread, it means the parent thread,
+ the main thread in this case (could be any thread which created the thread on which . join() is being called) is saying
+ "Hey thread, once you are done executing your task, join my flow of execution". It's like the parent thread waits for
+ the completion of the child thread and then continues with its execution.  */
+
 public class JoinThreadExample {
     public static void main(String[] args) throws InterruptedException {
         Thread one = new Thread(()->{
