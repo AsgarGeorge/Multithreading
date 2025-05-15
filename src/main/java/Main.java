1a637d1b09f321c1da25fbf5201e9cac2e5f62a9
Asgar_George <asgarantony@gmail.com>
2025-05-15 20:08:13 +0530

    join method

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main extends  Thread{
-    public static void main(String[] args) {
-        int core = Runtime.getRuntime().availableProcessors();
-        System.out.println(core);
-    }
-}
