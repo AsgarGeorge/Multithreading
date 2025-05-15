03f323a4e0046d63776de24f583ccbdc2d4fd8c6
Asgar_George <asgarantony@gmail.com>
2025-05-15 20:09:36 +0530

    join method

--- src/main/java/JoinThreadExample.java
+++ src/main/java/JoinThreadExample.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class JoinThreadExample {
+    public static void main(String[] args) throws InterruptedException {
+        Thread one = new Thread(()->{
+            for(int i = 0; i< 10; i++){
+                System.out.println("Thread One: "+ i);
+            }
+        });
+        Thread two = new Thread(()->{
+            for(int i = 0; i< 10; i++){
+                System.out.println("Thread Two: "+ i);
+            }
+        });
+
+        one.start();
+        two.start();
+        one.join();
+        two.join();
+        System.out.println("The execution of thread completed");
+    }
 }
